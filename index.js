7780aee172a1e204ae006ea3b9c2331117039bc1
Pete Hanner <petehanner@gmail.com>
2019-09-17 08:35:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(driverArray, str) {
+  return driverArray.filter(driver => {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+
+function fuzzyMatch(driverArray, str) {
+  const subString = str.length;
+  return driverArray.filter(driver => {
+    return driver.slice(0, subString).toLowerCase() === str.toLowerCase();
+  } );
+}
+
+function matchName(driverObjectArray, str) {
+  return driverObjectArray.filter(driver => {
+    return driver["name"].toLowerCase() === str.toLowerCase();
+  });
+}
